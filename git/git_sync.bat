28f7989bac420d71a443cca1e9ea93cac0f25594
Joel Van Eenwyk <joel.van.eenwyk@havok.com>
2013-07-03 16:59:41 -0700

    Added some messages to the script, handled some more edge cases, and added some comments.

--- git/git_sync.bat
+++ git/git_sync.bat
@@ -1,31 +1,35 @@
 @echo off
 
-set HOME=%~dp0
+::
+:: WARNING: This will overwrite existing changes to your source files
+::          and will delete the local .git repository.
+::
 
-set GIT_BIN=%~dp0\Tools\Git\bin
-set GIT=%GIT_ROOT%\git.exe
-
-:: Add git executable to the path
+set GIT_BIN=C:\Projects\ProjectAnarchy\Tools\PortableGit-1.8.0-preview20121022\bin
 set PATH=%GIT_BIN%;%PATH%
 
 set PA_GIT_SERVER_HTTPS=https://github.com/projectanarchy/projectanarchy.git
-set PA_GIT_SERVER=git@github.com:projectanarchy/projectanarchy.git
-set GIT_TEMP_FOLDER=_git_temp
+set GIT_TEMP_FOLDER=%~dp0_git_temp
 
+:: Remove the existing target folders if they exist
+echo Removing existing target folders...
+if exist .git rmdir /q /s .git
 if exist %GIT_TEMP_FOLDER% rmdir /q /s %GIT_TEMP_FOLDER%
-if exist .ssh (
-git clone --no-checkout %PA_GIT_SERVER% %GIT_TEMP_FOLDER%
-) else (
+
+echo Cloning Git repository into temporary folder...
 git clone --no-checkout %PA_GIT_SERVER_HTTPS% %GIT_TEMP_FOLDER%
-)
 
 if exist %GIT_TEMP_FOLDER% (
-set CURRENT_DIR=%CD%
+echo Moving Git repository to root of Anarchy SDK folder..
 cd %GIT_TEMP_FOLDER%
 mv .git ..
-cd %CURRENT_DIR%
+cd ..
 
 git reset --hard HEAD
 )
 
+echo Cleaning up temporary folder...
 if exist %GIT_TEMP_FOLDER% rmdir /q /s %GIT_TEMP_FOLDER%
+
+echo Finished syncing to newest source code!
+pause
